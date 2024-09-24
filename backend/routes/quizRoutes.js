a224c811e20855997bf718f75f9526fc3f579df9
Piotr Karyś <138370309+PiotrKarys@users.noreply.github.com>
2024-09-25 00:53:19 +0200

    Delete backend directory

--- backend/routes/quizRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getRandomQuestions } = require("../controllers/quizController");
-
-const router = express.Router();
-
-router.get("/questions/random", getRandomQuestions);
-
-module.exports = router;
